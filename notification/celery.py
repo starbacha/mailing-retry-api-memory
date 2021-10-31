36ef6a87ebd825b4c1c78b66164dafeb52c930fd
Aayush <44281902+aayushme@users.noreply.github.com>
2021-10-31 22:10:46 +0530

    Update celery.py

--- notification/celery.py
+++ notification/celery.py
@@ -4,3 +4,5 @@ os.environ.setdefaut('DJANGO_SETTING_MODULE','notification.settings')
 app = Celery('notification')
 app.config.from_object('django.conf.settings',namespace='CELERY')
 app.autodiscover_tasks()
+
+#import Os
