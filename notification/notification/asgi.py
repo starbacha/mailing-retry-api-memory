996c8522e7247b431cad241de410fc5f448511e8
Aayush <44281902+aayushme@users.noreply.github.com>
2021-10-31 21:55:47 +0530

    Update asgi.py

--- notification/notification/asgi.py
+++ notification/notification/asgi.py
@@ -5,6 +5,8 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+
+Contributed More
 """
 
 import os
